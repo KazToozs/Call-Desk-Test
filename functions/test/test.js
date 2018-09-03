b41beacefa8f8ed1f6484b72ac653ac2066c9433
Cristopher Toozs-Hobson <cris.hobson@hotmail.com>
2018-09-03 12:55:59 -0700

    good coverage

--- functions/test/test.js
+++ functions/test/test.js
@@ -1,9 +1,65 @@
-const assert = require('assert');
+// const _ = require('lodash');
+// const should = require('chai').should();
+const { expect } = require('chai');
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+const mocks = require('node-mocks-http');
+const assert = require('assert');
+const index = require('../index.js');
+const weather = require('../weather.js');
+
+// Test weather Promise
+describe('Working Promise', () => {
+  let maybe;
+  beforeEach(() => {
+    maybe = () => weather.callWeatherApi('San Francisco', '');
+  });
+  it('should be an Object', () => {
+    const result = maybe();
+    expect(result).to.be.a('Object');
+  });
+  it('should work', () => maybe());
+});
+
+// Index testing
+describe('Testing index', () => {
+  const request = mocks.createRequest({
+    body: {
+      queryResult: {
+        method: 'GET',
+        parameters: {
+          'geo-city': 'San Francisco',
+        },
+      },
+    },
+
+  });
+
+  const response = mocks.createResponse();
+  it('cannot find the weather', () => {
+    index.dialogflowFirebaseFulfillment(request, response);
+
+    assert.equal('I don\'t know the weather but I hope it\'s good!', response.fulfillmentText);
   });
 });
+
+
+// Potentially useful code below
+
+// describe('Promisestest', () => {
+//   let maybe;
+//   beforeEach(() => {
+//     maybe = () => Promise.resolve('yo');
+//   });
+//   it('should be an Object', () => {
+//     const result = maybe();
+//     _.isObject(result).should.be.true;
+//   });
+//   it('should work', () => maybe());
+//   it('should work and return data', () => maybe()
+//     .then((data) => {
+//       data.should.equal('yo');
+//     }));
+// });
+
+// const req = { query: { text: 'input' } };
+// const res = {};
